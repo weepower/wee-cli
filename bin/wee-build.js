d9eedb5b3bc35994aabbe98d82da89f515efb25a
Peter Bedor <peter@lewiscommunications.com>
2019-06-22 23:24:03 -0500

    Rework commands and structure

--- bin/wee-build.js
+++
@@ -1,8 +0,0 @@
-const commander = require('commander');
-const path = require('path');
-const program = new commander.Command();
-
-program.parse(process.argv);
-
-require(path.resolve(__dirname, '../lib/commands/build'))(program);
-
--- bin/wee.js
+++ bin/wee.js
@@ -1,22 +1,62 @@
 #! /usr/bin/env node
 
 const commander = require('commander');
-const chalk = require('chalk');
 const program = new commander.Command();
+const { error, log } = require('../lib/utils/log');
 
 program.version(require('../package').version);
 
-program
-    .command('build [env]', 'Build for the specified environment: development (default), staging, production');
+const Service = require('../lib/service/Service');
 
+const rawArgv = process.argv.slice(2);
+const args = require('minimist')(rawArgv, {
+    boolean: [
+        // build
+        'fail-silently',
+        'modern',
+        'report',
+        'report-json',
+        'watch',
+    ],
+});
+const command = args._[0];
+const options = {};
 
-// program
-//     .command('build <env>')
-//     .action((env) => {
+if (command === 'serve') {
+    options.plugins = [
+        './config/browser-sync'
+    ];
+}
 
-//     });
+const service = new Service(process.env.WEE_CLI_CONTEXT || process.cwd(), options);
 
-program.parse(process.argv)
+service.run(command, args, rawArgv).catch(err => {
+    let message = err;
+    let errors = null;
+
+    if (Array.isArray(err)) {
+        message = err[0];
+        errors = err[1];
+    }
+
+    log('\n\n');
+    error(message);
+
+    if (errors && errors.length) {
+        log('\n\n');
+        log(errors);
+    }
+
+    if (command !== 'serve') {
+        process.exit(1);
+    }
+})
+
+program.parse(process.argv);
+
+if (! process.argv.slice(2).length) {
+    program.outputHelp();
+}
 
 // init - new project
 // component - new component
